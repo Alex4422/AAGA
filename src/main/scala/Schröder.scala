a8777fdfd730f5d2fdc49519e4357787e2b33b10
Paul Charton <paul.charton@gmail.com>
2019-01-14 11:16:11 +0100

    On commence unrankTree, unrankComposition est fait

--- src/main/scala/Schröder.scala
+++ src/main/scala/Schröder.scala
@@ -1,6 +1,7 @@
 package schröder
 
 import scala.collection.mutable.ListBuffer
+import bib.Bib._
 
 class Schröder
 {
@@ -11,50 +12,99 @@ class Schröder
     def treeBuilder(n: Int): Schröder = //n -> Taille de l'arbre = nb Feuille
     {
         if (n <= 1) Leaf()
-
-        val rand = scala.util.Random
-        var T = Node(ListBuffer(Leaf(), Leaf()), 1)
-        var tmp = T
-        var last = T
-        var l = 2
-        var k = 0
-        var newNode = Leaf().asInstanceOf[Schröder]
-        var index = 0
-
-        var listLeaf = ListBuffer(T, T)
-
-        for(i <- 3 to n)
+        else
         {
-            k = rand.nextInt(i - 1)
-            if(k == i)
+            val rand = scala.util.Random
+            var T = Node(ListBuffer(Leaf(), Leaf()), 1)
+            var tmp = T
+            var last = T
+            var l = 2
+            var k = 0
+            var newNode = Leaf().asInstanceOf[Schröder]
+            var index = 0
+
+            var listLeaf = ListBuffer(T, T)
+
+            for(i <- 3 to n)
             {
-                last.fils.append(Leaf())
-                listLeaf.append(last)
+                k = rand.nextInt(i - 1)
+                if(k == i)
+                {
+                    last.fils.append(Leaf())
+                    listLeaf.append(last)
+                }
+                else
+                {
+                    println("k : "+k+" list : "+listLeaf.size)
+
+                    tmp = listLeaf.remove(k)
+
+                    for(j <- tmp.fils) j match
+                    {
+                        case Leaf() => newNode = j
+                        case _ =>
+                    }
+
+                    index = tmp.fils.indexOf(newNode)
+                    tmp.fils -= newNode
+                    tmp.fils.insert(index, Node(ListBuffer(Leaf(), Leaf()), l))
+                    last = tmp.fils.apply(index).asInstanceOf[Schröder.this.Node]
+
+                    listLeaf.insert(k, last)
+                    listLeaf.append(last)
+
+                    l += 1
+                }
+            }
+
+            T
+        }
+    }
+
+    def unrankTree(n: Int, s: Int): Schröder = //n -> Taille de l'arbre = nb Feuille, S -> Rank
+    {
+        if(n == 1) Leaf()
+        else
+        {
+            var k = n - 1
+            var gk = ComptageWeak(k)
+            var r = s
+
+            while(r >= 0)
+            {
+                r = r - combination(n - 1, k - 1) * gk
+                k = k - 1
+            }
+
+            k = k + 1
+            r = r + combination(n - 1, k - 1) * gk
+            var s2 = r % gk
+            var T = unrankTree(k, s2)
+            var C = unrankComposition(n, k, r/gk)
+        }
+    }
+
+    def unrankComposition(n: Int, k: Int, s: Int): ListBuffer[Int] =
+    {
+        if(n == 1 && k == 1 && s == 0) ListBuffer(1)
+        else
+        {
+            var tmp = 0
+            var s2 = s
+            var C = ListBuffer[Int]()
+
+            if(s2 < combination(n - 2, k - 1))
+            {
+                C = unrankComposition(n - 1, k, s2)
+                C.update(C.size, C.last + 1)
+                C
             }
             else
             {
-                println("k : "+k+" list : "+listLeaf.size)
-
-                tmp = listLeaf.remove(k)
-
-                for(j <- tmp.fils) j match
-                {
-                    case Leaf() => newNode = j
-                    case _ =>
-                }
-
-                index = tmp.fils.indexOf(newNode)
-                tmp.fils -= newNode
-                tmp.fils.insert(index, Node(ListBuffer(Leaf(), Leaf()), l))
-                last = tmp.fils.apply(index).asInstanceOf[Schröder.this.Node]
-
-                listLeaf.insert(k, last)
-                listLeaf.append(last)
-
-                l += 1
+                s2 = s2 - combination(n - 2, k - 1)
+                C = unrankComposition(n - 1, k, s2):+ 1
+                C
             }
         }
-
-        T
     }
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,36 +4,9 @@ import schröder.Schröder
 
 object main
 {
-    def combination(n: Int, k: Int): Int =
-    {
-        if (k == 0 || k == n) 1
-        else combination(n - 1, k - 1) + combination(n - 1, k)
-    }
-
-    def ComptageStong(n: Int, k: Int): Int = (n,k) match//n -> Feuille, k -> noeud interne
-    {
-        case (0, 0) => 0
-        case (1, 0) => 1
-        case (_, 1) => 1
-        case _ => ComptageStong(n - 1, k) + (n - 1) * ComptageStong(n - 1, k - 1)
-    }
-
-    def ComptageWeak(n: Int): Int =
-    {
-        if(n <= 0) 0
-        else if(n == 1) 1
-        else
-        {
-            var res = 0
-            for(k <- 1 to (n - 1)) res = combination(n - 1, k - 1) * ComptageWeak(k) + res
-            res
-        }
-    }
-
     def main(args: Array[String]): Unit =
     {
         val T = new Schröder
         println(T.treeBuilder(5))
     }
-
 }
